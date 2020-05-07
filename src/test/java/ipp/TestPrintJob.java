6b1f2b0e1c5fdda14dcf2be4d982e047ec42bfa3
Gerhard Muth <gerhard.muth@gmx.de>
2020-05-07 09:35:45 +0200

    manual tests with cups on localhost

---
+++ src/test/java/ipp/TestPrintJob.java
@@ -0,0 +1,17 @@
+package ipp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URI;
+
+public class TestPrintJob {
+
+  public static void main(String[] args) throws Exception {
+    URI printerURI = URI.create("http://localhost:631/printers/ColorJet_HP");
+    String userDesktop = System.getProperty("user.home") + "/Desktop";
+    File file = new File(userDesktop, "print.pdf");
+    new PrintJob()
+        .printDocument(printerURI, new FileInputStream(file));
+  }
+
+}
