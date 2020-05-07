7ff15c8d70d6b050ff96106fb5bbfefc973490d8
Gerhard Muth <gerhard.muth@gmx.de>
2020-05-07 09:44:51 +0200

    ipp scheme

--- src/test/java/ipp/TestPrintJob.java
+++ src/test/java/ipp/TestPrintJob.java
@@ -7,7 +7,7 @@ import java.net.URI;
 public class TestPrintJob {
 
   public static void main(String[] args) throws Exception {
-    URI printerURI = URI.create("http://localhost:631/printers/ColorJet_HP");
+    URI printerURI = URI.create("ipp://localhost:631/printers/ColorJet_HP");
     String userDesktop = System.getProperty("user.home") + "/Desktop";
     File file = new File(userDesktop, "print.pdf");
     new PrintJob()
